9bdd7a79f396bbce69a83d25f779acde54e91066
ThatianaMattos <thatianamattos25@gmail.com>
2025-11-04 19:58:10 -0300

    aula07: fila e pilha + ajustes (ExemploSet.ts, tsconfig etc)

--- aula06/ExemploSet.ts
+++ aula06/ExemploSet.ts
@@ -1,5 +1,14 @@
 // Cria a Collection Set, do tipo string, chamada frutas e inicializa
-const setFrutas: Set<string> = new Set<string>(["Abacate", "Banana", "Jaboticaba", "Kiwi", "Maçã", "Morango", "Pêra", "Jaboticaba"]);
+const setFrutas: Set<string> = new Set<string>([
+  "Abacate",
+  "Banana",
+  "Jaboticaba",
+  "Kiwi",
+  "Maçã",
+  "Morango",
+  "Pêra",
+  "Jaboticaba",
+]);
 
 /* Mostra na tela todas as frutas adicionadas.
    Observe que para exibir as frutas utilizamos 
@@ -7,9 +16,8 @@ const setFrutas: Set<string> = new Set<string>(["Abacate", "Banana", "Jaboticaba
 
 console.log("\nFrutas cadastradas: ");
 
-for(let fruta of setFrutas)
-{
-    console.log(fruta);
+for (let fruta of setFrutas) {
+  console.log(fruta);
 }
 
 /*Adiciona novas Frutas*/
@@ -19,14 +27,13 @@ setFrutas.add("Amora");
 setFrutas.add("Caju");
 
 /**
- * Mostra os dados armazenados na Collection Set Observe 
+ * Mostra os dados armazenados na Collection Set Observe
  * que a fruta repetida não foi inserida novamente
  */
 console.log("\nFrutas cadastradas: ");
 
-for(let fruta of setFrutas)
-{
-    console.log(fruta);
+for (let fruta of setFrutas) {
+  console.log(fruta);
 }
 
 // Verifica se um elemento está armazenado na Collection Set
@@ -37,9 +44,8 @@ setFrutas.delete("Kiwi");
 
 console.log("\nKiwi foi removida!\n");
 
-for(let fruta of setFrutas)
-{
-    console.log(fruta);
+for (let fruta of setFrutas) {
+  console.log(fruta);
 }
 
 // Verifica após a remoção se o elemento ainda existe na Collection Set
